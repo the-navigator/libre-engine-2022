2897b0aa60349a6363df2343d6502a601a423d8e
Team JAR <thenavigator.app15@gmail.com>
2022-03-08 14:54:13 +0800

    added new protocols for reading the database

--- LibreEngine/LibreEngine_Root.cs
+++ LibreEngine/LibreEngine_Root.cs
@@ -13,13 +13,27 @@ public class LibreEngine
     public static string databaseName; //Since this is designed to be flexible, it will adapt to the database name
     static string  connectionString = "DataSource=" +
      Environment.CurrentDirectory + "\\" + databaseName; 
-
+// Setting up some variables for the Datbase Connection
     SQLiteConnection databaseCn = new SQLiteConnection(connectionString);
+    SQLiteCommand databaseCommand = new SQLiteCommand();
+    SQLiteDataAdapter libreDataAdapter = new SQLiteDataAdapter();
+    public DataTable libreDataTable = new DataTable();
+
+    public int rowcount;
+
+     string tableSource = "ResourceDB";
+        
     
     public static void locateDatabase (string CurrentLocation)
     {
 
-        
+       databaseConnection.Open();   
+       libreDataAdapter = new SQLiteDataAdapter("SELECT * FROM [ResourceDB]", databaseCn);
+
+       libeDataAdapter.Fill(libreDataTable);
+       rowcount = libreDataTable.Rows.Count;
+       databaseCn.Close();
+
 
     }
     
