805f79ea622a9e6fa9e4661748e5c3d09bfbee48
Team JAR <thenavigator.app15@gmail.com>
2022-03-08 11:20:05 +0800

    Still looking for a way to integrate SQLite Library via VS Code

--- LibreEngine/LibreEngine_Root.cs
+++ LibreEngine/LibreEngine_Root.cs
@@ -1,5 +1,27 @@
-﻿namespace LibreEngine_Root;
-public static class LibreEngine
-{
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// using System.Data.SQLite;
+using System.Data;  
 
+
+namespace LibreEngine_Root;
+public class LibreEngine
+{
+    public static string databaseName; //Since this is designed to be flexible, it will adapt to the database name
+    static string  connectionString = "DataSource=" +
+     Environment.CurrentDirectory + "\\" + databaseName; 
+
+    SQLiteConnection databaseCn = new SQLiteConnection(connectionString);
+    
+    public static void locateDatabase (string CurrentLocation)
+    {
+
+        
+
+    }
+    
 }
+    
